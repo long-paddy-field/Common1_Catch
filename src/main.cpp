1206a66d6cfac39dfeec86e4cb1e91328d0aeea8
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2021-11-15 10:41:30 +0900

    yeah

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,17 @@ asm(".global _printf_float");
 #include"mbed.h"
 #include"controller.h"
 
-#define SVALUE
-#define USER_SW
-#define PWM_CH
-#define PWM_CW
+CAN can(PA_11,PA_12,500000);
+PwmOut Rail(PA_7);
+PwmOut Turn(PA_3);
+DigitalOut Rail_dir(PA_1);
+DigitalOut Turn_dir(PA_6);
 
-//CAN通信
-CAN can_con(PB_8,PB_9,500000);
-CAN can_info(PB_5,PB_6,500000);
 
-//電磁弁
-DigitalOut S_valve(SVALUE);
-
-//起動スイッチ（あれば）
-DigitalIn SW(USER_SW);
-
-//共通エリア用サーボモータ
-PwmOut C_hand(PWM_CH);
-PwmOut C_wrist(PWM_CW);
-
-//コントローラのコンストラクタ
-Controller controller(can_con,0x334);
-
+Controller controller(can,0x334);
 
 int main()
 {
-  printf("Program started\n");
-  while(SW);
 
-  
   return 0;
 }
